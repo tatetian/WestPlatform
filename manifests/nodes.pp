8277752ad6106aae1a31c4bbc14213275d8f9321
Tate, Hongliang Tian <tatetian@gmail.com>
2015-04-22 07:43:52 +0000

    Remove useless sample code

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,20 +1,3 @@
-node 'tian-PC' {
-    $site_domain = 'tian-PC.example.com'
-    
-    nginx::website { 'my-pc':
-        site_domain => 'tian-PC.example.com',
-    }
-    
-    exec { 'retrieve home page':
-        command => "wget -o stdout http://$site_domain > /dev/null",
-        path => ['/bin', '/usr/bin'],
-    }
-
-    class { 'ntp':
-        server => 'us.pool.ntp.org',
-    }
-}
-
 #hadoop params
 $h_version = '2.2.0'
 $h_group = 'hadoop'
